10e07b4b727f61b43a32805641d866f8e420cf1a
shamilkhan <shamilkhan1999@gmail.com>
2018-07-23 19:21:22 +0300

    Revert "site deploy"
    This reverts commit 8d5d6e38d88827f56b51f729178e9cc76f824f4c.

--- app/js/common.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var menu = document.getElementById('mobile-menu'),
-        menulink = document.getElementById('menu-burger');
-                
-    menulink.addEventListener('click', function(e) {
-        menu.classList.toggle('active');
-        e.preventDefault();
-    });
-})();
