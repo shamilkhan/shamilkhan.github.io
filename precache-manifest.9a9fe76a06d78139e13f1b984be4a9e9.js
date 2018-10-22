11d778dec8c6d7faf2ecbba7c3f4dc7775ab26ac
shamilkhan <shamilkhan1999@gmail.com>
2018-10-22 20:46:37 +0300

    1 version

---
+++ precache-manifest.9a9fe76a06d78139e13f1b984be4a9e9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "883f8e99781da5633250ca7a27bbc3b4",
+    "url": "/static/media/comment.883f8e99.png"
+  },
+  {
+    "revision": "0d999f43876c1effb853",
+    "url": "/static/css/main.80bd09db.chunk.css"
+  },
+  {
+    "revision": "8adf6245139065295ba8",
+    "url": "/static/js/1.8adf6245.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "72aad2c66183b24b50340c5badcbcb33",
+    "url": "/static/media/firstGroup.72aad2c6.png"
+  },
+  {
+    "revision": "4c8e1761e2a3117f45489b5e1380b495",
+    "url": "/static/media/secondGroup.4c8e1761.png"
+  },
+  {
+    "revision": "0d999f43876c1effb853",
+    "url": "/static/js/main.0d999f43.chunk.js"
+  },
+  {
+    "revision": "b87302823226ef852d3ed4407f4d7681",
+    "url": "/static/media/solarSystem.b8730282.png"
+  },
+  {
+    "revision": "d078a14d84c9a68ec1c9879c8ee01f96",
+    "url": "/static/media/transitionTop.d078a14d.png"
+  },
+  {
+    "revision": "6ee4c2532f73d6f96d170e7663edcdbd",
+    "url": "/static/media/diag.6ee4c253.png"
+  },
+  {
+    "revision": "e15efe2d47ff4ad6e2f53f1f0b29189a",
+    "url": "/static/media/moon.e15efe2d.png"
+  },
+  {
+    "revision": "05cbec7ababeabde6e1ad5d90011842f",
+    "url": "/static/media/transitionBottom.05cbec7a.png"
+  },
+  {
+    "revision": "d65176babaca6fd1b4869f0e40f5fd5d",
+    "url": "/index.html"
+  }
+];
