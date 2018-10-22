13736e62286ce2f379f140ab7f7ec1a4859cbef3
shamilkhan <shamilkhan1999@gmail.com>
2018-10-22 22:56:49 +0300

    new dep

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-import './styles/main.sass';
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
